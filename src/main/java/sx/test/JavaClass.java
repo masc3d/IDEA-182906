bff347da0a27f9a1c3db94408e8a546d8376e2d8
masc <masc@disappear.de>
2018-10-31 10:49:05 +0100

    INITIAL

---
+++ src/main/java/sx/test/JavaClass.java
@@ -0,0 +1,10 @@
+package sx.test;
+
+/**
+ * Created by masc on 31.10.18.
+ */
+public class JavaClass {
+    void test() {
+        System.out.println(this.getClass().toString());
+    }
+}
---
+++ src/main/kotlin/sx/test/KotlinClass.kt
@@ -0,0 +1,10 @@
+package sx.test
+
+/**
+ * Created by masc on 31.10.18.
+ */
+class KotlinClass {
+    fun test() {
+        println("${this.javaClass}")
+    }
+}
